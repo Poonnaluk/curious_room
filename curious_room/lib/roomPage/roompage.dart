60521554f72a8908392eef785f91b12c673268db
Siriwika <siriwika574@gmail.com>
2021-10-16 13:03:03 +0700

    folder roomPage

---
+++ curious_room/lib/roomPage/roompage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoomPage extends StatefulWidget {
+  const RoomPage({Key? key}) : super(key: key);
+
+  @override
+  _RoomPageState createState() => _RoomPageState();
+}
+
+class _RoomPageState extends State<RoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
