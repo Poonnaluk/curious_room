8ac98a4ef4b0dc646e480ed86ad9548728a29e20
Siriwika <siriwika574@gmail.com>
2021-10-17 17:44:38 +0700

    update roompage

--- curious_room/lib/room/roompage.dart
+++ curious_room/lib/room/roompage.dart
@@ -13,6 +13,9 @@ class _RoomPageState extends State<RoomPage> {
   late double screenw;
   late double screenh;
 
+  bool chooseNew = true;
+  bool chooseHots = false;
+
   @override
   Widget build(BuildContext context) {
     screenw = MediaQuery.of(context).size.width;
@@ -27,7 +30,9 @@ class _RoomPageState extends State<RoomPage> {
             widget.projectName,
           ),
           titleTextStyle: const TextStyle(
-              color: Colors.black, fontSize: 28, fontFamily: 'Prompt'),
+              color: Color.fromRGBO(176, 162, 148, 1),
+              fontSize: 28,
+              fontFamily: 'Prompt'),
           leading: Icon(
             Icons.menu,
             color: Colors.red,
@@ -49,7 +54,116 @@ class _RoomPageState extends State<RoomPage> {
             )
           ],
         ),
+        body: SafeArea(
+            child: Center(
+          child: Container(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [_buildButtonCreate()]),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(35, 5, 10, 0),
+                    child: _buttonNew('ล่าสุด', chooseNew),
+                  ),
+                  _buttonHots('ยอดนิยม', chooseHots),
+                ],
+              )
+            ],
+          )),
+        )),
       );
     }
+  }
+
+  Widget _buildButtonCreate() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(
+          screenw * 0.010, screenh * 0.02, screenw * 0.010, screenh * 0.01),
+      alignment: Alignment.center,
+      child: TextButton(
+        style: TextButton.styleFrom(
+            primary: Color.fromRGBO(107, 103, 98, 1),
+            backgroundColor: Colors.white,
+            padding: const EdgeInsets.only(
+                left: 66.5, top: 42.5, right: 306.5, bottom: 42.5),
+            shadowColor: Color.fromRGBO(176, 162, 148, 1),
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                side: BorderSide(
+                    color: Color.fromRGBO(176, 162, 148, 1), width: 1))),
+        child: Text(
+          'แชร์สิ่งที่กำลังสัยอยู่...',
+          style: TextStyle(fontSize: 18),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget _buttonNew(String textbutton, bool button) {
+    return Container(
+      child: TextButton(
+        style: TextButton.styleFrom(
+            primary: Color.fromRGBO(107, 103, 98, 1),
+            backgroundColor: Colors.white,
+            padding: const EdgeInsets.only(
+                left: 32.5, top: 21, right: 32.5, bottom: 21),
+            shadowColor: Color.fromRGBO(176, 162, 148, 1),
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                side: BorderSide(
+                    color: button
+                        ? Color.fromRGBO(225, 141, 63, 1)
+                        : Color.fromRGBO(176, 162, 148, 1),
+                    width: 3))),
+        child: Text(
+          textbutton,
+          style: TextStyle(fontSize: 18),
+        ),
+        onPressed: () {
+          setState(() {
+            chooseNew = true;
+            chooseHots = false;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buttonHots(String textbutton, bool button) {
+    return Container(
+      child: TextButton(
+        style: TextButton.styleFrom(
+            primary: Color.fromRGBO(107, 103, 98, 1),
+            backgroundColor: Colors.white,
+            padding: const EdgeInsets.only(
+                left: 32.5, top: 21, right: 32.5, bottom: 21),
+            shadowColor: Color.fromRGBO(176, 162, 148, 1),
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                side: BorderSide(
+                    color: button == true
+                        ? Color.fromRGBO(225, 141, 63, 1)
+                        : Color.fromRGBO(176, 162, 148, 1),
+                    width: 3))),
+        child: Text(
+          textbutton,
+          style: TextStyle(fontSize: 18),
+        ),
+        onPressed: () {
+          setState(() {
+            chooseHots = true;
+            chooseNew = false;
+          });
+        },
+      ),
+    );
   }
 }
