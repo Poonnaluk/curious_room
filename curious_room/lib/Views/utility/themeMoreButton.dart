f38e847f8ae65a65fce1ca3c9320d0669ee8de3a
Poonnaluk <610107030035@dpu.ac.th>
2021-11-15 16:02:27 +0700

    Edit post and Delete Post success

---
+++ curious_room/lib/Views/utility/themeMoreButton.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+Row themeMoreButton(String image, String name) {
+  return Row(
+    children: [
+      Container(
+        margin: EdgeInsets.all(14),
+        child: Image.asset(
+          image,
+          scale: 16,
+        ),
+      ),
+      Text(name,
+          style:
+              TextStyle(color: Color.fromRGBO(0, 0, 0, 0.6), fontSize: 20.sp)),
+    ],
+  );
+}
